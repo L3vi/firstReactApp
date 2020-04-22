18bf615de4872847c8d5f0e860e0c687b5faf4f1
L3vi <levistum@gmail.com>
2020-04-21 19:49:58 -0600

    First commit of first react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      classes: [],
+      classData: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://www.dnd5eapi.co/api/classes")
+      .then(res => {
+        this.setState({ isLoaded: true, classes: res.data.results });
+      })
+      .catch(err => this.setState({ error: err, isLoaded: true }));
+  }
+
+  getClassData(className) {
+    axios.get(`https://www.dnd5eapi.co/api/classes/${className}`)
+      .then(res => {
+        this.setState({ isLoaded: true, classData: res.data.name });
+      })
+      .catch(err => this.setState({ error: err, isLoaded: true }));
+  }
+
+  render() {
+    const { error, isLoaded, classes } = this.state;
+    if (error) {
+      return <div>Error: error.message</div>;
+    } else if (!isLoaded) {
+      return <div>Loading. . .</div>;
+    } else {
+      const navBar = <NavigationBar classes={classes} onClick={className => { this.getClassData(className) }}></NavigationBar>;
+      if (this.state.classData) {
+        return <div>{navBar}<br/>{this.state.classData}</div>;
+      } else {
+        return navBar;
+      }
+
+    }
+  }
+}
+
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: this.props.classes
+    }
+  }
+
+  render() {
+    return (<div className="navBar">
+      {this.state.classes.map(c => <NavigationButton key={c.index} value={c.name} onClick={() => { this.props.onClick(c.index) }}></NavigationButton>)}
+    </div>)
+  }
+
+}
+
+class NavigationButton extends React.Component {
+  render() {
+    return <button className="navButton" onClick={() => this.props.onClick()}>{this.props.value}</button>;
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Page />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
